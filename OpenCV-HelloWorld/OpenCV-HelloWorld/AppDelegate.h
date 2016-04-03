25edd5cd182cab6e518dbcdbcf4f8f7c6b7d741e
Lenny Boyatzis <lennyboyatzis@gmail.com>
2016-04-03 18:37:24 +1000

    initial commit

---
+++ OpenCV-HelloWorld/OpenCV-HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenCV-HelloWorld
+//
+//  Created by Leo Boyatzis on 3/04/2016.
+//  Copyright © 2016 Leo Boyatzis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenCV-HelloWorld/OpenCV-HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenCV-HelloWorld
+//
+//  Created by Leo Boyatzis on 3/04/2016.
+//  Copyright © 2016 Leo Boyatzis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
